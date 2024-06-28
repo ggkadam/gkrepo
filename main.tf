7a5a31849f0374e166aaac642a521a07405e6269
ggkadam <girishkadam2006@gmail.com>
2024-06-28 10:26:23 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,14 @@
+locals {
+  instance_type = {
+    default = "t2.nano"
+    dev     = "t2.micro"
+    prod    = "t2.small"
+  }
+ 
+}
+ 
+resource "aws_instance" "myec2" {
+  ami           = "ami-008c09a18ce321b3c"
+  instance_type  = "t2.micro"
+ 
+}
